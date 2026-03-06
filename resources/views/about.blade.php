f6432a3314e83663b48a7d638fc69f9aae20f1a0
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-06 15:37:39 +0700

    lesson3: routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About Us</h1>
+<h2>
+    <a href="{{route('contact')}}">Contact page</a>
+</h2>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route thường
+Route::get('/contact', function () {
+    return "Contact Us";
+})->name('contact');
+
+// Route trả về view
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Route có tham số, tham số phải là số nguyên
+Route::get('/users/{id}', function (int $id) {
+    return "User ID: {$id}";
+});
+
+// Route có tham số tùy chọn, nếu không có sẽ lấy giá trị mặc định
+Route::get('/students/{name?}', function (?string $name = 'TrongNghia') {
+    return "Student Name: {$name}";
+});
+
+// Route nhóm
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Admin Dashboard";
+    });
+
+    Route::get('/settings', function () {
+        return "Admin Settings";
+    });
+});
+
+// Route fallback
+Route::fallback(function () {
+    return "Không tìm thấy trang";
+});
